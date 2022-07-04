51f9c206bb29188fa6135c0d39f76664de1a6cf1
mjmolieleng@gmail.com <mjmolieleng@gmail.com>
2022-07-04 18:30:47 +0200

    Initial Commit

---
+++ Lucky Number/Lucky_NumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lucky_NumberApp.swift
+//  Lucky Number
+//
+//  Created by Mohale on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct Lucky_NumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lucky NumberUITests/Lucky_NumberUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lucky_NumberUITestsLaunchTests.swift
+//  Lucky NumberUITests
+//
+//  Created by Mohale on 2022/07/04.
+//
+
+import XCTest
+
+class Lucky_NumberUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
