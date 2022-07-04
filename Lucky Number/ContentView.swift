95556e648782ce7539a19e06a869983fe4f21fb0
mjmolieleng@gmail.com <mjmolieleng@gmail.com>
2022-07-04 18:32:09 +0200

    Initial Commit

--- Lucky Number/ContentView.swift
+++ Lucky Number/ContentView.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var runCount = generateNumber()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 50) {
+            Text("Lucky Number")
+                .font(.title)
+            
+            Text(String(runCount))
+                .font(.title)
+            
+            Button("Generate", action: {
+                runCount = generateNumber()
+                print("Some Number")
+            }).padding(12)
+                .background(Color(UIColor.systemIndigo))
+                .foregroundColor(Color.white)
+                .cornerRadius(8)
+                
+        }
+        .padding(10)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.yellow)
     }
+}
+
+func generateNumber() -> Int {
+    return Int(arc4random_uniform(100)) + 1
 }
 
 struct ContentView_Previews: PreviewProvider {
